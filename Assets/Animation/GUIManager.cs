c98202108b618602512211e363b36b7cf4454d43
Maxim Bondarchuk <valta91@mail.ru>
2018-05-11 22:47:03 +0300

    Добавил проект и ассеты

---
+++ Assets/Animation/GUIManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour {
+
+    public List<Sprite> healthBar = new List<Sprite>();
+
+
+    public Image healthBarGameobject;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void SetHealthBar(int value)
+    {
+        if (value > healthBar.Count)
+            return;
+
+
+        healthBarGameobject.sprite = healthBar[value];
+
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float axisX;
+    public float axisY;
+    public float amplitudeMove;
+
+
+
+
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        UpdateAxis();
+
+
+		
+		
+	}
+
+    private void UpdateAxis()
+    {
+        axisX = Input.GetAxis("Horizontal");
+        axisY = Input.GetAxis("Vertical");
+    }
+}
